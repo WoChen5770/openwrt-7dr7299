ab688a51bc31380a5a7ec7a79e0b89abdbbda428
WoChen5770 <jaycq.com@gmail.com>
2026-05-02 13:43:42 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -82,9 +82,6 @@ if [ -n "$DEFAULT_SETTINGS_FILE" ] && [ -f "$DEFAULT_SETTINGS_FILE" ]; then
   fi
 fi
 
-# 修复 hostapd 报错
-cp -f "$GITHUB_WORKSPACE/scripts/011-fix-mbo-modules-build.patch" package/network/services/hostapd/patches/011-fix-mbo-modules-build.patch
-
 # 修复 armv8 设备 xfsprogs 报错
 sed -i 's/TARGET_CFLAGS.*/TARGET_CFLAGS += -DHAVE_MAP_SYNC -D_LARGEFILE64_SOURCE/g' feeds/packages/utils/xfsprogs/Makefile
 
